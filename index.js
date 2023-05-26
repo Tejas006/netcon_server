21bb29ff627632c6f52bb67c84948f696133224d
Tejas06 <tejaspy24@gmail.com>
2023-05-26 19:06:03 +0530

    changes made

--- index.js
+++ index.js
@@ -22,9 +22,9 @@ mongoose.connect(process.env.MONGO_URL, {
 }).catch((err)=>{
     console.log(err.message)
 })
-
-const server = app.listen(process.env.PORT, () => {
-    console.log(`Server Started on Port ${process.env.PORT}`)
+const PORT = process.env.PORT || 5000
+const server = app.listen(PORT, () => {
+    console.log(`Server Started on Port ${PORT}`)
 })
 
 
